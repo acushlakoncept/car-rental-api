36c2094d341dfdae35b4ba61f0c78d70df96ad2a
Uduak Essien <acushla4real@gmail.com>
2021-01-15 16:19:53 +0100

    Add user model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true, length: { minimum: 3}
+    validates :password, presence: true, length: { minimum: 6 }
     has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:username) }
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_length_of(:username).
+          is_at_least(3)}
+
+
+it { should_not validate_length_of(:password).
+            is_at_least(5)}
 end
