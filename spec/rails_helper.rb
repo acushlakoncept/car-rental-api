757d04f9cd6d99164f85352c3e6488dab568e8ed
Uduak Essien <acushla4real@gmail.com>
2021-01-15 16:42:08 +0100

    Add RequestHelperSpec and config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,7 +29,7 @@ end
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -55,6 +55,7 @@ RSpec.configure do |config|
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
 
+  config.include RequestSpecHelper, type: :request
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
 
+    let!(:user1) { create_list(:user, 2) }
+
+    describe 'GET /api/v1/users' do
+        it 'returns status code 200' do
+            expect(response).to have_http_status(:success)
+        end
+        it 'returns list of users' do
+            expect(json.size).to eq(2)
+        end
+    end
 end
