cf0a252bb3e55548290ac025f1ff707015e8bc62
Uduak Essien <acushla4real@gmail.com>
2021-01-18 04:13:46 +0100

    Update cars request specs

--- spec/requests/cars_request_spec.rb
+++ spec/requests/cars_request_spec.rb
@@ -4,21 +4,30 @@ RSpec.describe 'Cars', type: :request do
   let!(:user1) { create_list(:user, 1) }
 
   describe 'GET /cars' do
-    before do
+    before(:each) do
+      FactoryBot.create(:user, username: 'user1', password: 'password')
       FactoryBot.create(:car, make: 'Toyota', model: 'Corolla',
                               year: 2011, color: 'Silver', transmission: 'Automatic',
-                              ac: true, max_capacity: 4, user: user1)
+                              ac: true, max_capacity: 4, user_id: 1)
 
       FactoryBot.create(:car, make: 'Nissan', model: 'Almera',
                               year: 2002, color: 'Blue', transmission: 'Manual',
-                              ac: true, max_capacity: 4, user_id: user1.id)
+                              ac: true, max_capacity: 4, user_id: 1)
     end
 
     it 'returns all cars' do
       get '/api/v1/cars'
 
       expect(response).to have_http_status(:success)
-      expect(response_body.size).to eq(2)
+      expect(json.size).to eq(2)
     end
+
+    it 'returns a subset of cars based on limit' do
+        get '/api/v1/cars', params: { limit: 1 }
+
+        expect(response).to have_http_status(:success)
+        expect(json.size).to eq(1)
+    end
+
   end
 end
