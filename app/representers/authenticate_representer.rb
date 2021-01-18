05b0ae23cb4e6e76c711c0861e9edf21f45037f5
Uduak Essien <acushla4real@gmail.com>
2021-01-18 16:06:33 +0100

    Add authenticate representer

---
+++ app/representers/authenticate_representer.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class AuthenticateRepresenter
+  attr_reader :user
+
+  def initialize(user)
+    @user = user
+  end
+
+  def as_json
+    {
+      id: user.id,
+      username: user.username,
+      admin: user.admin,
+      token: AuthenticationTokenService.call(user.id)
+    }
+  end
+end
