06722c7a3dd2e5bd0724eb72fe5b9c659ac624cc
Uduak Essien <acushla4real@gmail.com>
2021-01-18 02:36:04 +0100

    Update cars controller with index method

---
+++ app/controllers/api/v1/cars_controller.rb
@@ -0,0 +1,27 @@
+module Api
+  module V1
+    class CarsController < ApplicationController
+      MAX_PAGINATION_LIMIT = 100
+
+      def index; 
+        cars = Car.limit(limit).offset(params[:offset])
+
+        render json: CarsRepresenter.new(cars).as_json
+      end
+
+
+      private
+
+      def limit
+        [
+            params.fetch(:limit, MAX_PAGINATION_LIMIT).to_i,
+            MAX_PAGINATION_LIMIT
+        ].min
+      end
+
+      def car_params
+        params.require(:car).permit(:make, :model, :year, :color, :transmission, :ac, :max_capacity)
+      end
+    end
+  end
+end
