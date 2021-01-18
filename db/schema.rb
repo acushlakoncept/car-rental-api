254554ed63db52724ec096e537c17f4299f3059c
Uduak Essien <acushla4real@gmail.com>
2021-01-18 02:20:34 +0100

    Update users_request_spec

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,15 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_115_145_321) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 2021_01_18_010916) do
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'username'
-    t.string 'password_digest'
-    t.boolean 'admin', default: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "make"
+    t.string "model"
+    t.integer "year"
+    t.string "color"
+    t.string "transmission"
+    t.boolean "ac"
+    t.integer "max_capacity", default: 4
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cars_on_user_id"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.boolean "admin", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "cars", "users"
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -5,13 +5,13 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   let!(:user1) { create_list(:user, 2) }
 
-  describe 'GET /api/v1/users' do
-    it 'returns status code 200' do
-      expect(response).to have_http_status(:success)
-    end
+  describe 'GET /users' do
 
     it 'returns list of users' do
+      get '/api/v1/users'
+      expect(response).to have_http_status(:success)
       expect(json.size).to eq(2)
     end
+
   end
 end
