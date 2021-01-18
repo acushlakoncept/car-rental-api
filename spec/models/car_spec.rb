c453d16f2f4cb4e12c766d01c6ba51c4fb93e19b
Uduak Essien <acushla4real@gmail.com>
2021-01-18 02:23:19 +0100

    Add car unit testings

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:make) }
+  it { should validate_presence_of(:model) }
+  it { should validate_presence_of(:year) }
+  it { should validate_presence_of(:color) }
+  it { should validate_presence_of(:transmission) }
+  it { should validate_presence_of(:ac) }
 end
