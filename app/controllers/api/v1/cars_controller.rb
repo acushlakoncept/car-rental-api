710b683c64d14f0461529c1164195c1ce2e8e2f8
Uduak Essien <acushla4real@gmail.com>
2021-01-18 02:50:23 +0100

    Add FactoryBot for car

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -3,24 +3,24 @@ module Api
     class CarsController < ApplicationController
       MAX_PAGINATION_LIMIT = 100
 
-      def index; 
+      def index
         cars = Car.limit(limit).offset(params[:offset])
 
         render json: CarsRepresenter.new(cars).as_json
       end
 
-
       private
 
       def limit
         [
-            params.fetch(:limit, MAX_PAGINATION_LIMIT).to_i,
-            MAX_PAGINATION_LIMIT
+          params.fetch(:limit, MAX_PAGINATION_LIMIT).to_i,
+          MAX_PAGINATION_LIMIT
         ].min
       end
 
       def car_params
-        params.require(:car).permit(:make, :model, :year, :color, :transmission, :ac, :max_capacity)
+        params.require(:car).permit(:make, :model, :year,
+                                    :color, :transmission, :ac, :max_capacity)
       end
     end
   end
---
+++ spec/factories/car.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+    factory :car do
+    end
+  end
+  
