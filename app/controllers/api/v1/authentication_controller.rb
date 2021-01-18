6e6fa788c2670c89f4800248c7558cc2353e4906
Uduak Essien <acushla4real@gmail.com>
2021-01-18 03:15:59 +0100

    Add authentication controller and request spec file

---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,7 @@
+
+module Api
+    module V1
+      class AuthenticationController < ApplicationController
+      end
+    end
+end
---
+++ spec/requests/authentication_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Authentications", type: :request do
+
+end
