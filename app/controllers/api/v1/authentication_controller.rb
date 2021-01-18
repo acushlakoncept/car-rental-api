8266486fc054c4bb3b929dd3c1488b10c9f4428d
Uduak Essien <acushla4real@gmail.com>
2021-01-18 15:58:03 +0100

    Add authentication controller

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,7 +1,32 @@
+# frozen_string_literal: true
 
 module Api
-    module V1
-      class AuthenticationController < ApplicationController
+  module V1
+    class AuthenticationController < ApplicationController
+      
+      rescue_from ActionController::ParameterMissing, with: :parameter_missing
+      rescue_from AuthenticateError, with: :handle_unauthenticated
+
+      def create
+        raise AuthenticateError unless user.authenticate(params.require(:password))
+
+        render json: AuthenticateRepresenter.new(user).as_json, status: :created
       end
+
+      private
+
+      def user
+        @user ||= User.find_by(username: params.require(:username))
+      end
+
+      def parameter_missing(e)
+        render json: { error: e.message }, status: :unprocessable_entity
+      end
+
+      def handle_unauthenticated
+        head :unauthorized
+      end
+
     end
+  end
 end
