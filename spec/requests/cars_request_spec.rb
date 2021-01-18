4ae91b6b68a4328d481408afc018afa0c1aab8fb
Uduak Essien <acushla4real@gmail.com>
2021-01-18 18:09:17 +0100

    Update cars requests spec

--- spec/requests/cars_request_spec.rb
+++ spec/requests/cars_request_spec.rb
@@ -1,32 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'Cars', type: :request do
-  let!(:user1) { create_list(:user, 1) }
+  let!(:user1) { create_list(:user, 5) }
 
   describe 'GET /cars' do
     before(:each) do
-      FactoryBot.create(:user, username: 'user1', password: 'password')
+
       FactoryBot.create(:car, make: 'Toyota', model: 'Corolla',
                               year: 2011, color: 'Silver', transmission: 'Automatic',
-                              ac: true, max_capacity: 4, user_id: 1)
+                              ac: 'true', max_capacity: 4, user_id: 1,
+                              image_url: 'https://source.unsplash.com/800x600/?car,silver')
 
-      FactoryBot.create(:car, make: 'Nissan', model: 'Almera',
-                              year: 2002, color: 'Blue', transmission: 'Manual',
-                              ac: true, max_capacity: 4, user_id: 1)
     end
 
     it 'returns all cars' do
       get '/api/v1/cars'
 
-      expect(response).to have_http_status(:success)
-      expect(json.size).to eq(2)
+      expect(response).to have_http_status(:unauthorized)
+      expect(json.size).to eq(1)
     end
 
-    it 'returns a subset of cars based on limit' do
-        get '/api/v1/cars', params: { limit: 1 }
+    it 'returns all users' do
+      get '/api/v1/users'
 
-        expect(response).to have_http_status(:success)
-        expect(json.size).to eq(1)
+      expect(response).to have_http_status(:success)
     end
 
   end
