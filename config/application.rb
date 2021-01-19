61937b1569043dbe82490cc84d2d31d4c37f5e94
Uduak Essien <acushla4real@gmail.com>
2021-01-19 10:29:25 +0100

    Add rack-cors configuration

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,11 @@ module CarRentals
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: %i[get patch put delete post options]
+      end
+    end
   end
 end
