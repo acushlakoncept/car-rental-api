699d93a5901971f26c9e5081dc65864e0d955451
Uduak Essien <acushla4real@gmail.com>
2021-01-22 00:22:39 +0100

    Generate favourite controller with spec files

---
+++ spec/factories/favourites.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :favourite do
+    user { "" }
+    car { "" }
+  end
+end
---
+++ spec/requests/favourites_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Favourites", type: :request do
+
+end
