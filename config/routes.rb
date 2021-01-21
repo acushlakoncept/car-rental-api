11a897eae931f13cd1cc3033e4e5ce9b1f26487d
Uduak Essien <acushla4real@gmail.com>
2021-01-22 00:53:24 +0100

    Add favourites route resources

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: :index
+      resources :favourites, only: :create
       resources :cars, only: %i[index create show destroy]
 
       post 'login', to: 'authentication#create'
