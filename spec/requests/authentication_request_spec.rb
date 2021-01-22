ccdcb3f7a15e048d12f0e8622c41b1600f665707
Uduak Essien <acushla4real@gmail.com>
2021-01-22 12:13:02 +0100

    Add authentication spec for user login

--- spec/requests/authentication_request_spec.rb
+++ spec/requests/authentication_request_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Authentications', type: :request do
+  describe 'POST /authenticate' do
+    let(:user) { FactoryBot.create(:user, username: 'acushla', password: 'password') }
+
+    it 'authenticates the user' do
+      post '/api/v1/login', params: { username: user.username, password: 'password' }
+
+      expect(response).to have_http_status(:created)
+      expect(json).to eq({
+                           'id' => 1,
+                           'username' => 'acushla',
+                           'admin' => false,
+                           'token' => AuthenticationTokenService.call(1)
+                         })
+    end
+  end
 end
