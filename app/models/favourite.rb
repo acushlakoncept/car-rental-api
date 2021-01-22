42143bd35710ec5ec7d26229345126034a930936
Uduak Essien <acushla4real@gmail.com>
2021-01-22 10:33:42 +0100

    Add model validation for favourite uniqueness

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,4 +1,8 @@
 class Favourite < ApplicationRecord
   belongs_to :user
   belongs_to :car
+
+  # validates :car_id, uniqueness: { scope: :user_id }
+  validates_uniqueness_of :car, scope: :user_id,
+  message: 'You already have it in your list of favourites'
 end
