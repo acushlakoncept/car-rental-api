12ed4b7723684fe3872cc359879787feb4bbf47e
Uduak Essien <acushla4real@gmail.com>
2021-01-22 12:22:03 +0100

    Add specs for incorrect username and password

--- spec/requests/authentication_request_spec.rb
+++ spec/requests/authentication_request_spec.rb
@@ -15,5 +15,23 @@ RSpec.describe 'Authentications', type: :request do
                            'token' => AuthenticationTokenService.call(1)
                          })
     end
+
+    it 'returns error when username does not exist' do
+      post '/api/v1/login', params: { username: 'ac', password: 'password' }
+
+      expect(response).to have_http_status(:unauthorized)
+      expect(json).to eq({
+                           'error' => 'No such user'
+                         })
+    end
+
+    it 'returns error when password is incorrect' do
+      post '/api/v1/login', params: { username: user.username, password: 'incorrect' }
+
+      expect(response).to have_http_status(:unauthorized)
+      expect(json).to eq({
+                           'error' => 'Incorrect password '
+                         })
+    end
   end
 end
