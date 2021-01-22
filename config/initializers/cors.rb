56de90c4bc70045e3c8dc924e9ae865740d8a425
Uduak Essien <acushla4real@gmail.com>
2021-01-22 20:24:59 +0100

    Change specific url to *

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'hire-a-car-client.herokuapp.com'
+    origins '*'
 
     resource '*',
       headers: :any,
