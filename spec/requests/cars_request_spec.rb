a57889e16516ba03457c24f213177f79f9c7d84c
Uduak Essien <acushla4real@gmail.com>
2021-01-22 12:29:29 +0100

    Add get /api/v1/cars spec

--- spec/requests/cars_request_spec.rb
+++ spec/requests/cars_request_spec.rb
@@ -1,27 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'Cars', type: :request do
-  let!(:user1) { create_list(:user, 5) }
+  let(:user) { FactoryBot.create(:user, username: 'acushla', password: 'password') }
 
   describe 'GET /cars' do
-    before(:each) do
+    before do
       FactoryBot.create(:car, make: 'Toyota', model: 'Corolla',
                               year: 2011, color: 'Silver', transmission: 'Automatic',
-                              ac: 'true', max_capacity: 4, user_id: 1,
+                              ac: 'true', max_capacity: 4, user: user,
                               image_url: 'https://source.unsplash.com/800x600/?car,silver')
     end
 
-    it 'returns all cars' do
+    it 'returns error when there is no current logged in user' do
       get '/api/v1/cars'
 
       expect(response).to have_http_status(:unauthorized)
-      expect(json.size).to eq(1)
+      expect(json).to eq({
+        'error' => 'You will need to login first'
+      })
     end
 
-    it 'returns all users' do
-      get '/api/v1/users'
+    # it 'returns all users' do
+    #   get '/api/v1/users'
 
-      expect(response).to have_http_status(:success)
-    end
+    #   expect(response).to have_http_status(:success)
+    # end
   end
 end
