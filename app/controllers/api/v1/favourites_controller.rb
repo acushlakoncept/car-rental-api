563aba382cb3ac10883a0c62d23375a73273098c
Uduak Essien <acushla4real@gmail.com>
2021-01-22 02:17:06 +0100

    Add index and destroy method for favourite

--- app/controllers/api/v1/favourites_controller.rb
+++ app/controllers/api/v1/favourites_controller.rb
@@ -3,18 +3,32 @@ module Api
     class FavouritesController < ApplicationController
       before_action :authenticate_request!
 
-      def create
-        current_user = current_user!
+      def index
+        favourites = User.fav_cars(current_user!)
 
-        favourite = current_user.favourites.create(car_id: params[:car_id])
-    
+        render json: CarsRepresenter.new(favourites).as_json
+      end
+
+      def create
+        favourite = current_user!.favourites.create(car_id: params[:car_id])
+
         if favourite.save
           render json: { message: 'Added to favourite succesfully' }, status: :created
         else
           render json: { error: 'Something went wrong' }, status: :unprocessable_entity
         end
       end
-      
+
+      def destroy
+        favourite = Favourite.find_by(id: params[:id], user: current_user!, car_id: params[:car_id])
+
+        if favourite
+          favourite.destroy
+          render json: { message: 'Car removed successfully from favorite' }
+        else
+          render json: { message: 'Something went wrong' }
+        end
+      end
     end
   end
 end
